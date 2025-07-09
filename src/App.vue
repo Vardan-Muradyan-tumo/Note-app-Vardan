380c6c4da4295fd5184fd61950bdb4c23ce77d8e
Vardan-Muradyan-tumo <vardan.muradyan2@tumo.org>
2025-07-09 20:19:56 +0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import PighCounter from './components/pighCounter.vue'
+import { ref } from 'vue';
+
+const arr = ref([{
+  name: 'Luna',age: 5, count: 2
+},{
+  name: 'Mbappe',age: 26, count: 9
+}])
+
+</script>
+
+<template>
+  <PighCounter v-for="pigh in arr" :name="pigh.name" :age="pigh.age" :count="pigh.count"/>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/pighCounter.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="card">
+    <h1>{{ props.name }}</h1>
+    <h2>{{ props.age }}</h2>
+<h1>{{ counter }}</h1>
+<button @click="pighShatacnel">click me</button>   
+</div>
+
+</template>
+<script setup>
+import{ref, defineProps} from 'vue'
+const props = defineProps(['name', 'age'])
+
+
+
+let counter = ref(props.age)
+function pighShatacnel(){
+    counter.value++
+}
+</script>
+<style scoped>
+.card{
+    width: 300px;
+    height: 300px;
+    border: 1px solid gray;
+    border-radius: 6px;
+    display: flex;
+    align-items:center ;
+    justify-content: center;
+    flex-direction: column;
+}
+</style>
